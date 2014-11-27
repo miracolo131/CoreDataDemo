f20efd3f11d0c7d662ef79fcb5755332c066b931
Miracolo Bosco <miracolo@MiracolodeMacBook-Air.local>
2014-11-27 11:06:19 +0900

    Initial Commit

---
+++ CoreData/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreData
+//
+//  Created by Miracolo Bosco on 14/11/27.
+//  Copyright (c) 2014年 MI-31. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
